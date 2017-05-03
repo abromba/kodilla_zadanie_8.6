aed9f98c40ebaf18a21f3cea067cb1faeed38fe3
Ania <Ania>
2017-05-03 11:21:19 +0200

    sprawdzenie wyniku : dodatni/ujemny/zero

--- scripts.js
+++ scripts.js
@@ -1,13 +1,48 @@
 var a = prompt('Podaj jakąś liczbę A');
 var b = prompt('Podaj jakąś liczbę B');
-var wynik = [(a * a) + (2 * a * b) - (b * b)];
+var result = [(a * a) + (2 * a * b) - (b * b)];
 
-alert('wynik działania (a * a) + (2 * a * b) - (b * b) to: ' + wynik );
-console.log('wynik działania (a * a) + (2 * a * b) - (b * b) to: ' + wynik);
+alert('wynik działania (a * a) + (2 * a * b) - (b * b) to: ' + result );
+console.log('wynik działania (a * a) + (2 * a * b) - (b * b) to: ' + result);
 
-if (wynik=0) {
-	alert('wynik to 0!')
-	console.log('wynik działania to 0')
-} else { alert('wynik nie równa się 0!')
-console.log('wynik działania nie równa sié 0')
+if (result>=0) {
+    alert('wynik jest dodatni')
+    console.log('wynik jest dodatni')
+} else if (result<=0){
+    alert('wynik jest ujemny')
+    console.log('wynik jest ujemny')
+} else if (result==0){
+    alert('wynik to 0!')
+    console.log('wynik działania to 0')
 }
+
+
+/*	
+var a = prompt('Podaj jakąś liczbę A');
+var b = prompt('Podaj jakąś liczbę B');
+var result = (a * a) + (2 * a * b) - (b * b);
+
+alert('policzę ile to jest (a * a) + (2 * a * b) - (b * b), daj mi chwilę ');
+console.log('policzę ile to jest (a * a) + (2 * a * b) - (b * b), daj mi chwilę');
+
+if (a==NaN)||(b==NaN){
+    alert('Błąd: to co wpisałeś nie było liczbą');
+    console.log('Błąd: to co wpisałeś nie było liczbą');
+    return "zacznij jeszcze raz";
+} 
+if (a==Number) && (b==Number){
+    alert('wynik działania (a * a) + (2 * a * b) - (b * b) to: ' + result );
+    console.log('wynik działania (a * a) + (2 * a * b) - (b * b) to: ' + result);
+}
+
+if (result>=0) {
+    alert('wynik jest dodatni');
+    console.log('wynik jest dodatni');
+} else if (result<=0){
+    alert('wynik jest ujemny');
+    console.log('wynik jest ujemny');
+} else if (result==0){
+    alert('wynik to 0!');
+    console.log('wynik działania to 0');
+}
+*/
