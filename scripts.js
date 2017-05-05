1ae2444f7f1ef99470c3667fbcdc1ef0f5244637
Ania <Ania>
2017-05-05 22:42:32 +0200

    Number.isNaN added

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
-var a = prompt('Podaj jakąś liczbę A');
+/*var a = prompt('Podaj jakąś liczbę A');
 var b = prompt('Podaj jakąś liczbę B');
-var result = [(a * a) + (2 * a * b) - (b * b)];
+var result = (a * a) + (2 * a * b) - (b * b);
 
 alert('wynik działania (a * a) + (2 * a * b) - (b * b) to: ' + result );
 console.log('wynik działania (a * a) + (2 * a * b) - (b * b) to: ' + result);
@@ -16,21 +16,26 @@ if (result>=0) {
     console.log('wynik działania to 0')
 }
 
-
-/*	
-var a = prompt('Podaj jakąś liczbę A');
-var b = prompt('Podaj jakąś liczbę B');
+*/
+	
+var a = Number(prompt('Podaj jakąś liczbę A'));
+var b = Number(prompt('Podaj jakąś liczbę B'));
 var result = (a * a) + (2 * a * b) - (b * b);
 
 alert('policzę ile to jest (a * a) + (2 * a * b) - (b * b), daj mi chwilę ');
 console.log('policzę ile to jest (a * a) + (2 * a * b) - (b * b), daj mi chwilę');
 
-if (a==NaN)||(b==NaN){
-    alert('Błąd: to co wpisałeś nie było liczbą');
-    console.log('Błąd: to co wpisałeś nie było liczbą');
-    return "zacznij jeszcze raz";
+if (Number.isNaN(a)) {
+    alert('Błąd: A nie było liczbą');
+    console.log('Błąd: A nie było liczbą');
+	//return a;
 } 
-if (a==Number) && (b==Number){
+else if(Number.isNaN(b)) {
+    alert('Błąd: B nie było liczbą');
+    console.log('Błąd: B nie było liczbą');
+	//return b;
+}
+else {
     alert('wynik działania (a * a) + (2 * a * b) - (b * b) to: ' + result );
     console.log('wynik działania (a * a) + (2 * a * b) - (b * b) to: ' + result);
 }
@@ -38,11 +43,10 @@ if (a==Number) && (b==Number){
 if (result>=0) {
     alert('wynik jest dodatni');
     console.log('wynik jest dodatni');
-} else if (result<=0){
+} else if (result<=0) {
     alert('wynik jest ujemny');
     console.log('wynik jest ujemny');
-} else if (result==0){
+} else {
     alert('wynik to 0!');
     console.log('wynik działania to 0');
 }
-*/
