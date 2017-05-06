d4026c7bd5507731d9140ed01f59960e8833375f
Ania <Ania>
2017-05-06 17:02:05 +0200

    rebuilding if

--- scripts.js
+++ scripts.js
@@ -1,10 +1,9 @@
 var a = Number(prompt('Podaj jakąś liczbę A'));
 var b = Number(prompt('Podaj jakąś liczbę B'));
 var result = (a * a) + (2 * a * b) - (b * b);
-
-console.log('Policzę ile to jest (a * a) + (2 * a * b) - (b * b), daj mi chwilę...');
-
-console.log(
-    (Number.isNaN(a)) ? 'Błąd: A nie było liczbą :(': ((Number.isNaN(b)) ? 'Błąd: B nie było liczbą :(' :  'W ynik działania to: ' + result ));
-console.log(
-    result === 0 ? 'Wynik to 0!' : result < 0 ? 'Wynik jest ujemny.' : (Number.isNaN(result)) ? 'Nie mogę tego policzyć, bo pomyliłeś się wpisując dane. Spróbuj jeszcze raz!' : 'Wynik jest dodatni.');
+if (Number.isNaN(a&&b)){
+	console.log('Nie mogę tego policzyć, bo pomyliłeś się wpisując dane. Spróbuj jeszcze raz!');
+} else {
+	console.log('wynik działania (a * a) + (2 * a * b) - (b * b) to: ' + result);
+	console.log(result === 0 ? 'Wynik to 0!' : result < 0 ? 'Wynik jest ujemny.' : 'Wynik jest dodatni.');
+}
